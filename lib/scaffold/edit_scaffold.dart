47a26714c67767a4d89b43e22734754544585703
Hiromu Nakano <hiromu.nakano.12@gmail.com>
2020-07-18 06:26:37 +0900

    Chage EditScaffold to Form

--- lib/scaffold/edit_scaffold.dart
+++ lib/scaffold/edit_scaffold.dart
@@ -1,84 +1,83 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qrstocker/entity/item.dart';
 import 'package:qrstocker/model/database.dart';
 
 class EditScaffold extends StatelessWidget {
   EditScaffold(this._item)
-      : _noteController = TextEditingController(
+      : _titleController = TextEditingController(
+          text: _item.title,
+        ),
+        _noteController = TextEditingController(
           text: _item.note,
         );
 
   final Item _item;
+  final TextEditingController _titleController;
   final TextEditingController _noteController;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_item.title),
-        leading: IconButton(
-          icon: Icon(Icons.close),
-          onPressed: () => Navigator.pop(context),
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_item.title),
+          leading: IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () => Navigator.pop(context),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => _showDeleteDialog(context),
+            ),
+          ],
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () => _showDeleteDialog(context),
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.fromLTRB(
-            40,
-            20,
-            40,
-            20,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SelectableText(_item.data),
-              Expanded(
-                child: Center(
-                  child: Card(
-                    color: Colors.white,
-                    child: Container(
-                      constraints: BoxConstraints(
-                        maxWidth: 200,
-                        maxHeight: 200,
-                      ),
-                      child: QrImage(
-                        data: _item.data,
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(
+              40,
+              20,
+              40,
+              20,
+            ),
+            child: Form(
+              child: ListView(
+                children: <Widget>[
+                  FormField(
+                    builder: (_) => TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Title',
                       ),
                     ),
                   ),
-                ),
+                  FormField(
+                    builder: (_) => TextField(
+                      controller: _noteController,
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        labelText: 'Note',
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              TextField(
-                controller: _noteController,
-                textAlign: TextAlign.center,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Done',
-        child: Icon(Icons.done),
-        onPressed: () {
-          _save();
-          Navigator.popUntil(
-            context,
-            (route) => route.isFirst,
-          );
-        },
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Done',
+          child: Icon(Icons.done),
+          onPressed: () {
+            _save();
+            Navigator.popUntil(
+              context,
+              (route) => route.isFirst,
+            );
+          },
+        ),
       ),
     );
   }
